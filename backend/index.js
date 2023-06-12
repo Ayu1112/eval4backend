5da8114231474435bcc1d589937290fa4f87c3fa
Ayush Kumar Pandey <badsoulak@gmail.com>
2023-06-12 17:01:01 +0530

    commit

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express=require('express');
+const {connection} = require('./db');
+const { postRouter } = require('./routes/posts.route');
+const { userRouter } = require('./routes/user.route');
+const cors=require("cors")
+app.use(cors())
+
+const app = express();
+app.use(express.json());
+app.use('/users',userRouter);
+app.use('/post',postRouter);
+require("dotenv").config();
+
+
+
+app.listen(process.env.port,async()=>{
+try {
+    await connection
+    console.log(`listening on port${process.env.port}`);
+    console.log("connected to db");
+} catch (err) {
+    console.log(err);
+    console.log("something went wrong");
+}
+})
